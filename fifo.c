c20a04de1b955fdd9089dcf9bcd71304bb4461b6
204G1A0533 <112563839+204G1A0533@users.noreply.github.com>
2022-11-01 12:36:58 +0530

    gnapika bai m written her details

--- fifo.c
+++ fifo.c
@@ -1,3 +1,12 @@
+/*Roll No: 204G1A0501
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 29-08-2022
+   Date of Execution: 29-08-2022
+*/
+
+
+
+
 #include<stdio.h>
 #include<conio.h> int fr[3];
 void main()
