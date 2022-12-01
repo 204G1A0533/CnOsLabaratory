3516adb5fab85ca65556f21aa1207519b64154fb
204G1A0533 <112563839+204G1A0533@users.noreply.github.com>
2022-12-01 15:23:57 +0530

    Gnapika Bai.M  Created Bankers.c

---
+++ Bankers.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: Gnapika Bai.M
+   Roll No: 204G1A0533
+   Title of the Experiment: Bankers Algorithm DeadLock Avoidance
+   Date of Creation: 01-12-2022
+   Date of Execution: 01-12-2022
+*/
